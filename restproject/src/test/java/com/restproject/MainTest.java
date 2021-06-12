29d8a2908123898cc6ab876358b539ce99172ea5
Gobinda Roy <Gobinda.Roy0406@gmail.com>
2021-06-12 13:39:21 +0530

    updated

--- restproject/src/test/java/com/restproject/MainTest.java
+++ restproject/src/test/java/com/restproject/MainTest.java
@@ -23,7 +23,7 @@ public class MainTest {
 		 Response response = httpRequest.request(Method.GET, "/Hyderabad");
 		 int statusCode = response.getStatusCode();
 		 Assert.assertEquals(statusCode, 200);
-		 //Small changes done
+		 //Small changes done12
 
 
 
