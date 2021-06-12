ed02978c91c938c36a4ed637cb55231334a2b6d9
Gobinda Roy <Gobinda.Roy0406@gmail.com>
2021-06-12 23:35:27 +0530

    updated pom

--- pipeline-backend-main/src/main/java/com/example/EmployeeService/EmployeeServiceApplication.java
+++ pipeline-backend-main/src/main/java/com/example/EmployeeService/EmployeeServiceApplication.java
@@ -2,13 +2,21 @@ package com.example.EmployeeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeServiceApplication {
+public class EmployeeServiceApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(EmployeeServiceApplication.class);
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
 	
 }
+
