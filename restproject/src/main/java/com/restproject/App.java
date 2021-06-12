c13bacc12ec474ce3ec79ab36c0bb7fce5811952
Gobinda Roy <63896476+Gobinda0406@users.noreply.github.com>
2021-06-12 13:28:26 +0530

    Add files via upload

---
+++ restproject/src/main/java/com/restproject/App.java
@@ -0,0 +1,13 @@
+package com.restproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
